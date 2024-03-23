6e7452a0d8ed9272fb0470622f5a9c6bf71e783f
unknown <sharma.shivam2110@gmail.com>
2024-03-23 13:52:59 +0530

    Write logic for update,delete,getallproduct,findbyid

--- controllers/productController.js
+++ controllers/productController.js
@@ -36,3 +36,72 @@ exports.createProduct = async (req, res, next) => {
       .json({ status: 'fail', message: 'Internal server error' });
   }
 };
+
+exports.getProductById = async (req,res,next)=>{
+  const {id} = req.params.id;
+
+  try {
+    
+  const product = await product.findById(id);
+  res.status(200).json(product);
+    
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+};
+
+exports.getProducts = async (req,res,next)=>{
+
+  try {
+
+    const products = await Product.find();
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(400).send(error);
+
+  }
+  
+};
+
+exports.deleteProduct = async(req,res,next)=>{
+  const {id} = req.params.id;
+
+  try {
+    const product = await findById(id);
+
+    if(!product){
+      return res.status(400).send("Product Not Found");
+    }else{
+      product.remove();
+      res.status(200).send("Product Removed Succesfullly",id);
+    }
+
+
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.updateCourse = async(req,res,next)=>{
+  const {id} = req.params.id;
+  const data = req.body;
+
+  try {
+    const product = await Product.findById(id);
+    if(!product){
+      res.status(400).send("Product Not Found")
+    }
+
+    const updateProduct = await Product.findByIdAndUpdate(
+      id,
+      data,
+      {new:true}
+    );
+    
+    res.status(200).send(updateProduct);
+
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
